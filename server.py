f3dff1a4f0c780f860637824b53828761fa1eaa4
vqyuan <52634862+vqyuan@users.noreply.github.com>
2019-09-12 21:08:15 -0700

    Add files via upload
    Fixed server

--- server.py
+++ server.py
@@ -4,11 +4,6 @@ import urllib.parse
 import token_generation as g
 import token_verification as v
 
-# Reading index.html
-f = open('client.html')
-html_page = f.read()
-f.close()
-
 class Handler(http.server.BaseHTTPRequestHandler):
     # Function for the headers
     def _set_headers(self):
@@ -16,32 +11,31 @@ class Handler(http.server.BaseHTTPRequestHandler):
         self.send_header('Content-type', 'text/html')
         self.end_headers()
 
-    # Display webpage on startup at localhost:8000/client.html and verify JWT
+    # Display webpage on startup at localhost:8000 and verify JWT
     def do_GET(self):
         try:
-            # Displaying webpage
-            if self.path.endswith('.html'):
-                self._set_headers()
-                try: 
-                    self.wfile.write(html_page.encode())
-                except:
-                    message = "An error has occured with returning the webpage"
-                    self.wfile.write(message.encode())
-                return
-            # Verifying JWT
-            else:
-                self._set_headers()
+            self._set_headers()
+            if 'Authorization' in self.headers:
                 try:
                     token = str(self.headers['Authorization'])
                     if v.verify_token(token) is True:
-                        message = "Token successfully verified"
+                        message = "Token was successfully verified"
                         self.wfile.write(message.encode())
                     else:
                         message = "Token could not be verified"
                         self.wfile.write(message.encode())
                     return
                 except:
-                    message = "No token could be found"
+                    message = "Token could not be verified"
+                    self.wfile.write(message.encode())
+            else:
+                try: 
+                    f = open('client.html')
+                    html_page = f.read()
+                    f.close()
+                    self.wfile.write(html_page.encode())
+                except:
+                    message = "An error has occured with returning the webpage"
                     self.wfile.write(message.encode())
         except:
             self.send_response(500)
@@ -51,8 +45,6 @@ class Handler(http.server.BaseHTTPRequestHandler):
 
     # How to handle the submitted form
     def do_POST(self):
-        self._set_headers()
-
         # Content length
         content_length = int(self.headers['Content-Length'])
         # Content data
@@ -63,14 +55,15 @@ class Handler(http.server.BaseHTTPRequestHandler):
 
         # Comparing the credentials to see if they are true
         # This is where it would query the database if I had one
-        if credentials['firstname'] == 'John' and credentials['lastname'] == 'Doe':
+        if credentials['username'] == 'user' and credentials['password'] == 'pass':
             # Actual generation of the token
-            token = g.generate_token(credentials['firstname'], credentials['lastname'])
+            self._set_headers()
+            token = g.generate_token(credentials['username'], credentials['password'])
             self.wfile.write(token)
             return
         else:
-            message = "Sorry, but your credentials could not be verified"
-            self.wfile.write(message.encode())
+            self.send_response(401)
+            self.end_headers()
             return
             
         return
